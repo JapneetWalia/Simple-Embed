dc2108bf8deb8ef5acf13688dc26c7d1c2a47a8e
Japneet Singh <34630853+JapneetWalia@users.noreply.github.com>
2020-11-09 16:46:52 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,11 +12,11 @@ from shutil import rmtree
 from setuptools import find_packages, setup, Command
 
 # Package meta-data.
-NAME = 'mypackage'
+NAME = 'SimpleEmbedding'
 DESCRIPTION = 'My short description for my project.'
-URL = 'https://github.com/me/myproject'
-EMAIL = 'me@example.com'
-AUTHOR = 'Awesome Soul'
+URL = 'https://github.com/JapneetWalia/SimpleEmbedding'
+EMAIL = 'japneetwalia2797@gmail.com'
+AUTHOR = 'Japneet Singh'
 REQUIRES_PYTHON = '>=3.6.0'
 VERSION = '0.1.0'
 
