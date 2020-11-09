b4e2f56e5effdb1fb5b2681927f4886c48328a2a
Japneet Singh <34630853+JapneetWalia@users.noreply.github.com>
2020-11-09 18:12:38 +0000

    Delete SimpleEmbed.py

--- Env/SimpleEmbed.py
+++
@@ -1,5 +0,0 @@
-def getDependencies():
-  !pip install git+https://github.com/UKPLab/sentence-transformers
-  !pip install embedding_as_service
-  !pip install laserembeddings
-  !python -m laserembeddings download-models
